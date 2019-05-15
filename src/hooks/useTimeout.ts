0d8045666bfdbfafd9eeff350e45c2bedbe90bf5
jxom <jakemoxey@gmail.com>
2019-05-16 00:49:32 +1000

    fix

--- src/hooks/useTimeout.ts
+++ src/hooks/useTimeout.ts
@@ -3,13 +3,10 @@ import * as React from 'react';
 export default function useTimeout() {
   const timeout = React.useRef<number | undefined>(undefined);
 
-  const _setTimeout = React.useCallback(
-    (fn, ms: number) => {
-      // @ts-ignore
-      timeout.current = setTimeout(fn, ms);
-    },
-    []
-  );
+  const _setTimeout = React.useCallback((fn, ms: number) => {
+    // @ts-ignore
+    timeout.current = setTimeout(fn, ms);
+  }, []);
   const _clearTimeout = React.useCallback(() => clearTimeout(timeout.current), []);
 
   React.useEffect(() => {
